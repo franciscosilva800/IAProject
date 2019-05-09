f8e795c03b0aaf680bf2a5d22a293bc982618880
franciscosilva800 <franciscosilva800@gmail.com>
2019-05-09 16:40:06 +0100

    [02]Adicionei código ao construtor do CatchProblemSearch

--- src/catchBox/CatchProblemSearch.java
+++ src/catchBox/CatchProblemSearch.java
@@ -1,17 +1,27 @@
 package catchBox;
 
+import agentSearch.Action;
 import agentSearch.Problem;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CatchProblemSearch<S extends CatchState> extends Problem<S> {
     //TODO this class might require the definition of additional methods and/or attributes
 
+    private final CatchState goalState;
+    private ArrayList<Action> actions;
+
     public CatchProblemSearch(S initialCatchState, Cell goalPosition) {
         super(initialCatchState);
+        initialCatchState.setGoal(goalPosition.getLine(),goalPosition.getColumn());
+        this.goalState = initialCatchState;
+        actions = new ArrayList<>(4);
+        actions.add(new ActionUp());
+        actions.add(new ActionRight());
+        actions.add(new ActionDown());
+        actions.add(new ActionLeft());
 
-        //TODO
-        throw new UnsupportedOperationException("Not implemented at");
     }
 
     @Override
@@ -21,6 +31,6 @@ public class CatchProblemSearch<S extends CatchState> extends Problem<S> {
     }
 
     public boolean isGoal(S state) {
-        return true;
+        return goalState.equals(state);
     }
 }
