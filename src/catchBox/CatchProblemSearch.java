433b1b99d62c07411f97651abadc52ca29d1488a
franciscosilva800 <franciscosilva800@gmail.com>
2019-05-10 13:32:34 +0100

    Adiconei codigo à classe CatchProblemSearch

--- src/catchBox/CatchProblemSearch.java
+++ src/catchBox/CatchProblemSearch.java
@@ -9,28 +9,41 @@ import java.util.List;
 public class CatchProblemSearch<S extends CatchState> extends Problem<S> {
     //TODO this class might require the definition of additional methods and/or attributes
 
-    private final CatchState goalState;
+    //private final CatchState goalState;
     private ArrayList<Action> actions;
 
     public CatchProblemSearch(S initialCatchState, Cell goalPosition) {
         super(initialCatchState);
         initialCatchState.setGoal(goalPosition.getLine(),goalPosition.getColumn());
-        this.goalState = initialCatchState;
+        //this.goalState = new CatchState(new)
         actions = new ArrayList<>(4);
         actions.add(new ActionUp());
         actions.add(new ActionRight());
         actions.add(new ActionDown());
         actions.add(new ActionLeft());
 
+
     }
 
     @Override
     public List<S> executeActions(S state) {
-        //TODO
-        throw new UnsupportedOperationException("Not implemented at");
+        ArrayList<S> sucessors = new ArrayList<>(4);
+        S sucessor;
+        for (Action action : actions) {
+            if(action.isValid(state)){
+                sucessor = (S) state.clone();
+                action.execute(sucessor);
+                sucessors.add(sucessor);
+
+            }
+        }
+
+        /*DEVOLVER LISTA DE ESTADOS SUCESSORES*/
+        return sucessors;
     }
 
     public boolean isGoal(S state) {
-        return goalState.equals(state);
+        //return goalState.equals(state);
+        return true;
     }
 }
